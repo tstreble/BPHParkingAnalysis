83d6ec0735240748c6cfe312a49f390a8ca4dba1
Thomas Strebler <t.strebler@imperial.ac.uk>
2018-06-02 19:21:46 +0100

    Added BToKee and BToKmumu ntuple producers

---
+++ NtupleProducer/BToKmumuNtupleProducer.cc
@@ -0,0 +1,351 @@
+// Author: T. Strebler (IC)
+// Date:   31 May 2018
+//
+// Add new variables from NanoAOD tree for BToKpipi analysis
+// Can be either included in a interpreted macro or compiled in c++
+// (use `root-config --glibs --cflags`)
+//
+
+
+
+#include <iostream>
+#include "TFile.h"
+#include "TTree.h"
+#include "TChain.h"
+#include "TString.h"
+#include "TLorentzVector.h"
+
+#include "NanoAODTree.h"
+
+
+float JPsiMass_ = 3.0969;
+float BuMass_ = 5.279;
+float KaonMass_ = 0.493677;
+float MuonMass_ = 0.10565837;
+
+
+int main(int argc, char** argv) {
+
+  string status_sample = *(argv + 1);
+  bool isMC = false;
+  bool isData = false;
+  if (status_sample.compare("mc") == 0) isMC = true;
+  if (status_sample.compare("data") == 0) isData = true;
+
+
+  string output;
+  for (int i = 1; i < argc; ++i) {
+    if(std::string(argv[i]) == "--output") {
+      if (i + 1 < argc) {
+	output = argv[i+1];
+	break;
+      } else {
+	std::cerr << "--output option requires one argument." << std::endl;
+	return 1;
+      }      
+    }  
+  }
+  if(output==""){
+    std::cerr << "--output argument required" << std::endl;
+    return 1;
+  }
+    
+  
+
+  string input;
+  for (int i = 1; i < argc; ++i) {
+    if(std::string(argv[i]) == "--input") {
+      if (i + 1 < argc) {
+	input = argv[i+1];
+	break;
+      } else {
+	std::cerr << "--intput option requires one argument." << std::endl;
+	return 1;
+      }      
+    }  
+  }
+  if(input==""){
+    std::cerr << "--input argument required" << std::endl;
+    return 1;
+  }
+
+
+  bool overwrite;
+  for (int i = 1; i < argc; ++i) {
+    if(std::string(argv[i]) == "--overwrite") {
+      overwrite = true;
+      break;
+    }
+  }
+
+
+  bool saveFullNanoAOD;
+  for (int i = 1; i < argc; ++i) {
+    if(std::string(argv[i]) == "--saveFullNanoAOD") {
+      saveFullNanoAOD = true;
+      break;
+    }
+  }
+ 
+
+  TFile* f_new = TFile::Open(output.c_str());
+  if(f_new!=0 && !overwrite){
+    cout<<output<<" already exists, please delete it before converting again"<<endl;
+    return 0;
+  }
+  f_new = TFile::Open(output.c_str(),"RECREATE");
+
+
+  TChain* oldtree = new TChain("Events");
+  oldtree->Add(input.c_str());
+  NanoAODTree* tree = new NanoAODTree(oldtree);
+
+  TTree* tree_new=new TTree("BToKmumuTree","BToKmumuTree");
+  if(saveFullNanoAOD)
+    tree_new=tree->GetTree()->CloneTree(0);
+
+
+  //New branches
+
+  int _Muon_sel_index = -1;
+  int _BToKmumu_sel_index = -1;
+
+  tree_new->Branch("Muon_sel_index",&_Muon_sel_index,"Muon_sel_index/I");
+  tree_new->Branch("BToKmumu_sel_index",&_BToKmumu_sel_index,"BToKmumu_sel_index/I");
+
+  int _GenPart_BToKmumu_index = -1;
+  int _GenPart_JPsiFromB_index = -1;
+  int _GenPart_KFromB_index = -1;
+  int _GenPart_mu1FromJPsi_index = -1;
+  int _GenPart_mu2FromJPsi_index = -1;
+  int _BToKmumu_gen_index = -1;
+
+  if(isMC){
+
+    tree_new->Branch("GenPart_BToKmumu_index",&_GenPart_BToKmumu_index,"GenPart_BToKmumu_index/I");
+    tree_new->Branch("GenPart_JPsiFromB_index",&_GenPart_JPsiFromB_index,"GenPart_JPsiFromB_index/I");
+    tree_new->Branch("GenPart_KFromB_index",&_GenPart_KFromB_index,"GenPart_KFromB_index/I");
+    tree_new->Branch("GenPart_mu1FromJPsi_index",&_GenPart_mu1FromJPsi_index,"GenPart_mu1FromJPsi_index/I");
+    tree_new->Branch("GenPart_mu2FromJPsi_index",&_GenPart_mu2FromJPsi_index,"GenPart_mu2FromJPsi_index/I");
+    tree_new->Branch("BToKmumu_gen_index",&_BToKmumu_gen_index,"BToKmumu_gen_index/I");
+
+  }
+
+  int nentries = tree->GetEntries();
+  cout<<"Nentries="<<nentries<<endl;
+  cout<<"isMC="<<isMC<<endl;
+
+  for (int iEntry = 0; iEntry < nentries ; iEntry++){
+
+    tree->GetEntry(iEntry);
+
+    if(iEntry%10000==0) cout<<"Entry #"<<iEntry<<" "<< int(100*float(iEntry)/nentries)<<"%"<<endl;
+
+    _Muon_sel_index = -1;
+    _BToKmumu_sel_index = -1;
+
+    _GenPart_BToKmumu_index = -1;
+    _GenPart_JPsiFromB_index = -1;
+    _GenPart_KFromB_index = -1;
+    _GenPart_mu1FromJPsi_index = -1;
+    _GenPart_mu2FromJPsi_index = -1;
+    _BToKmumu_gen_index = -1;
+
+    //Select the muon
+
+    int nMuon = tree->nMuon;
+
+    for(int i_mu=0; i_mu<nMuon; i_mu++){
+
+      //Selections on the muon to refine (in particular trigger matching)
+      if( 1 ){
+	_Muon_sel_index = i_mu;
+	break; //Take leading muon passing the selections (muons are pt-ordered)
+      }
+
+    }
+
+    if(_Muon_sel_index <0){ //Should implement something to avoid overlap with BToKmm final state (nMuon<2 won't work because only muons with pT>3 GeV are stored in default NanoAOD)
+      tree_new->Fill();
+      continue;
+    }
+
+    //Select the BToKmumu candidate with reco criteria
+
+    int nBToKmumu = tree->nBToKmumu;
+    float best_JPsi_mass = -1.;
+    float best_Bu_mass = -1.;
+
+    for(int i_BToKmumu=0; i_BToKmumu<nBToKmumu; i_BToKmumu++){            
+
+      if(tree->BToKmumu_kaon_charge[i_BToKmumu]*tree->Muon_charge[_Muon_sel_index]>0) continue; //Only consider BToKmumu with opposite charge to muon
+      
+      TLorentzVector mu_sel;
+      TLorentzVector mu1;
+      TLorentzVector mu2;
+
+      mu_sel.SetPtEtaPhiM(tree->Muon_pt[_Muon_sel_index],
+			  tree->Muon_eta[_Muon_sel_index],
+			  tree->Muon_phi[_Muon_sel_index],
+			  tree->Muon_mass[_Muon_sel_index]);
+
+      mu1.SetPtEtaPhiM(tree->BToKmumu_mu1_pt[i_BToKmumu],
+		       tree->BToKmumu_mu1_eta[i_BToKmumu],
+		       tree->BToKmumu_mu1_phi[i_BToKmumu],
+		       MuonMass_);
+
+      mu2.SetPtEtaPhiM(tree->BToKmumu_mu2_pt[i_BToKmumu],
+		       tree->BToKmumu_mu2_eta[i_BToKmumu],
+		       tree->BToKmumu_mu2_phi[i_BToKmumu],
+		       MuonMass_);
+
+      if(mu1.DeltaR(mu_sel)<0.1 || mu2.DeltaR(mu_sel)<0.1) continue; //Avoid any ambibuity between tag muon and probe muons
+
+      float mumu_mass = tree->BToKmumu_mumu_mass[i_BToKmumu];
+      float mumu_CL_vtx = tree->BToKmumu_mumu_CL_vtx[i_BToKmumu];
+	
+      //JPsi selection
+      if( !(best_JPsi_mass < 0. 
+	    || abs(best_JPsi_mass-mumu_mass)<1e-3 //Several BToKmumu can share the same JPsi->mumu
+	    || abs(mumu_mass-JPsiMass_) < abs(best_JPsi_mass-JPsiMass_)) )       
+	continue;
+
+      //if( mumu_CL_vtx < min_CL) continue; //cut on mumu vtx refitting
+
+      float B_mass = tree->BToKmumu_mass[i_BToKmumu];
+      float B_CL_vtx = tree->BToKmumu_CL_vtx[i_BToKmumu];
+      
+      if( !(best_Bu_mass < 0. 
+	    || abs(B_mass-BuMass_) < abs(best_Bu_mass-BuMass_)) )       
+	continue;
+      
+      best_JPsi_mass = mumu_mass;
+      best_Bu_mass = B_mass;
+      _BToKmumu_sel_index = i_BToKmumu;
+
+    }
+
+
+    
+    //Select the BToKmumu candidate based on gen matching
+
+    if(isMC){
+      
+      int nGenPart = tree->nGenPart;
+      
+      for(int i_Bu=0; i_Bu<nGenPart; i_Bu++){
+
+	if(abs(tree->GenPart_pdgId[i_Bu])==521){
+
+	  for(int i_gen=0; i_gen<nGenPart; i_gen++){
+	    int pdgId = tree->GenPart_pdgId[i_gen];
+	    int mother_index = tree->GenPart_genPartIdxMother[i_gen];
+	    if(abs(pdgId)==443 && mother_index == i_Bu)
+	      _GenPart_JPsiFromB_index = i_gen;
+	    else if(abs(pdgId)==321 && mother_index == i_Bu)
+	      _GenPart_KFromB_index = i_gen;
+	    if(_GenPart_JPsiFromB_index>=0 && _GenPart_KFromB_index>=0){
+	      _GenPart_BToKmumu_index = i_Bu;
+	      break;
+	    }
+	  }	  
+
+	}
+	
+	if(_GenPart_BToKmumu_index>=0) break;
+
+      }
+
+
+      
+      for(int i_gen=0; i_gen<nGenPart; i_gen++){
+
+	int pdgId = tree->GenPart_pdgId[i_gen];
+	int mother_index = tree->GenPart_genPartIdxMother[i_gen];
+	if(abs(pdgId)==13 && mother_index == _GenPart_JPsiFromB_index && _GenPart_mu1FromJPsi_index<0)
+	  _GenPart_mu1FromJPsi_index = i_gen;
+	else if(abs(pdgId)==13 && mother_index == _GenPart_JPsiFromB_index)
+	  _GenPart_mu2FromJPsi_index = i_gen;
+	if(_GenPart_mu1FromJPsi_index>=0 && _GenPart_mu2FromJPsi_index>=0) break;
+
+      }
+
+      //mu1FromJPsi stored a leading daughter
+      if(tree->GenPart_pt[_GenPart_mu2FromJPsi_index]>tree->GenPart_pt[_GenPart_mu1FromJPsi_index]){
+	int i_temp = _GenPart_mu1FromJPsi_index;
+	_GenPart_mu1FromJPsi_index = _GenPart_mu2FromJPsi_index;
+	_GenPart_mu2FromJPsi_index = i_temp;
+      }
+
+
+      TLorentzVector gen_KFromB_tlv;
+      TLorentzVector gen_mu1FromJPsi_tlv;
+      TLorentzVector gen_mu2FromJPsi_tlv;
+      
+      gen_KFromB_tlv.SetPtEtaPhiM(tree->GenPart_pt[_GenPart_KFromB_index],
+				   tree->GenPart_eta[_GenPart_KFromB_index],
+				   tree->GenPart_phi[_GenPart_KFromB_index],
+				   KaonMass_);
+      gen_mu1FromJPsi_tlv.SetPtEtaPhiM(tree->GenPart_pt[_GenPart_mu1FromJPsi_index],
+				       tree->GenPart_eta[_GenPart_mu1FromJPsi_index],
+				       tree->GenPart_phi[_GenPart_mu1FromJPsi_index],
+				       MuonMass_);
+      gen_mu2FromJPsi_tlv.SetPtEtaPhiM(tree->GenPart_pt[_GenPart_mu2FromJPsi_index],
+				       tree->GenPart_eta[_GenPart_mu2FromJPsi_index],
+				       tree->GenPart_phi[_GenPart_mu2FromJPsi_index],
+				       MuonMass_);
+
+      float best_dR = -1.;
+
+      for(int i_BToKmumu=0; i_BToKmumu<nBToKmumu; i_BToKmumu++){
+
+	TLorentzVector kaon_tlv;
+	TLorentzVector mu1_tlv;
+	TLorentzVector mu2_tlv;
+	
+	kaon_tlv.SetPtEtaPhiM(tree->BToKmumu_kaon_pt[i_BToKmumu],
+			      tree->BToKmumu_kaon_eta[i_BToKmumu],
+			      tree->BToKmumu_kaon_phi[i_BToKmumu],
+			      KaonMass_);
+	mu1_tlv.SetPtEtaPhiM(tree->BToKmumu_mu1_pt[i_BToKmumu],
+			      tree->BToKmumu_mu1_eta[i_BToKmumu],
+			      tree->BToKmumu_mu1_phi[i_BToKmumu],
+			      MuonMass_);
+	mu2_tlv.SetPtEtaPhiM(tree->BToKmumu_mu2_pt[i_BToKmumu],
+			      tree->BToKmumu_mu2_eta[i_BToKmumu],
+			      tree->BToKmumu_mu2_phi[i_BToKmumu],
+			      MuonMass_);
+
+	float dR_KFromB = kaon_tlv.DeltaR(gen_KFromB_tlv);
+	float dR_mu1FromJPsi = min(mu1_tlv.DeltaR(gen_mu1FromJPsi_tlv),mu2_tlv.DeltaR(gen_mu1FromJPsi_tlv));
+	float dR_mu2FromJPsi = min(mu1_tlv.DeltaR(gen_mu2FromJPsi_tlv),mu2_tlv.DeltaR(gen_mu2FromJPsi_tlv));
+	//Should check that same objects not selected twice
+
+	float dR_tot = dR_KFromB + dR_mu1FromJPsi + dR_mu2FromJPsi; //In case several BToKmumu matches, take the closest one in dR_tot
+
+	if( dR_KFromB<0.1 && dR_mu1FromJPsi<0.1 && dR_mu2FromJPsi<0.1
+	    && (best_dR<0. || dR_tot<best_dR) ){
+	  best_dR = dR_tot;
+	  _BToKmumu_gen_index = i_BToKmumu;	  
+	}
+
+      }
+
+    }
+
+    tree_new->Fill();
+
+  }
+
+
+  f_new->cd();
+  if(!saveFullNanoAOD) tree_new->AddFriend("Events",input.c_str());
+
+  tree_new->Write();
+  f_new->Close();
+  return 0;
+
+}
+
+
+
--- NtupleProducer/NanoAODTree.h
+++ NtupleProducer/NanoAODTree.h
@@ -21,6 +21,8 @@
 
 const int kMuonMax = 100;
 const int kBToKpipiMax = 10000;
+const int kBToKmumuMax = 10000;
+const int kBToKeeMax = 10000;
 const int kGenPartMax = 10000;
 
 using namespace std;
@@ -34,7 +36,7 @@ public :
    int luminosityBlock;
    int event;
 
-   int nMuon;
+   uint nMuon;
    int Muon_charge[kMuonMax];
    float Muon_pt[kMuonMax];
    float Muon_eta[kMuonMax];
@@ -45,7 +47,7 @@ public :
    float Muon_pfRelIso04_all[kMuonMax];
    bool Muon_mediumId[kMuonMax];
 
-   int nBToKpipi;
+   uint nBToKpipi;
    float BToKpipi_CL_vtx[kBToKpipiMax];
    float BToKpipi_Kpi_CL_vtx[kBToKpipiMax];
    float BToKpipi_Kpi_mass[kBToKpipiMax];
@@ -61,6 +63,38 @@ public :
    float BToKpipi_piD0_eta[kBToKpipiMax];
    float BToKpipi_piD0_phi[kBToKpipiMax];
  
+   uint nBToKmumu;
+   float BToKmumu_CL_vtx[kBToKmumuMax];
+   float BToKmumu_mumu_CL_vtx[kBToKmumuMax];
+   float BToKmumu_mumu_mass[kBToKmumuMax];
+   float BToKmumu_mass[kBToKmumuMax];
+   int BToKmumu_kaon_charge[kBToKmumuMax];
+   float BToKmumu_kaon_pt[kBToKmumuMax];
+   float BToKmumu_kaon_eta[kBToKmumuMax];
+   float BToKmumu_kaon_phi[kBToKmumuMax];
+   float BToKmumu_mu1_pt[kBToKmumuMax];
+   float BToKmumu_mu1_eta[kBToKmumuMax];
+   float BToKmumu_mu1_phi[kBToKmumuMax];
+   float BToKmumu_mu2_pt[kBToKmumuMax];
+   float BToKmumu_mu2_eta[kBToKmumuMax];
+   float BToKmumu_mu2_phi[kBToKmumuMax];
+
+   uint nBToKee;
+   float BToKee_CL_vtx[kBToKeeMax];
+   float BToKee_ee_CL_vtx[kBToKeeMax];
+   float BToKee_ee_mass[kBToKeeMax];
+   float BToKee_mass[kBToKeeMax];
+   int BToKee_kaon_charge[kBToKeeMax];
+   float BToKee_kaon_pt[kBToKeeMax];
+   float BToKee_kaon_eta[kBToKeeMax];
+   float BToKee_kaon_phi[kBToKeeMax];
+   float BToKee_ele1_pt[kBToKeeMax];
+   float BToKee_ele1_eta[kBToKeeMax];
+   float BToKee_ele1_phi[kBToKeeMax];
+   float BToKee_ele2_pt[kBToKeeMax];
+   float BToKee_ele2_eta[kBToKeeMax];
+   float BToKee_ele2_phi[kBToKeeMax];
+
    int nGenPart;
    int GenPart_pdgId[kGenPartMax];
    int GenPart_genPartIdxMother[kGenPartMax];
@@ -82,7 +116,7 @@ public :
 
 NanoAODTree::NanoAODTree (TChain* tree)
 {
-    Init(tree);
+  Init(tree);
 }
 
 
@@ -97,6 +131,7 @@ void NanoAODTree::Init(TChain* tree)
   if (!tree) return;
   _tree = tree;  
   _tree->SetMakeClass(1); // needed especially when compiling
+  _tree->GetEntries(); //ROOT issue with TChain which only works after calling GetEntries... (sigh)
   
   _tree->SetBranchAddress("run",&run);
   _tree->SetBranchAddress("luminosityBlock",&luminosityBlock);
@@ -112,27 +147,67 @@ void NanoAODTree::Init(TChain* tree)
   _tree->SetBranchAddress("Muon_pfRelIso04_all",&Muon_pfRelIso04_all);
   _tree->SetBranchAddress("Muon_mediumId",&Muon_mediumId);
 
-  _tree->SetBranchAddress("nBToKpipi",&nBToKpipi);
-  _tree->SetBranchAddress("BToKpipi_CL_vtx",&BToKpipi_CL_vtx);
-  _tree->SetBranchAddress("BToKpipi_Kpi_CL_vtx",&BToKpipi_Kpi_CL_vtx);
-  _tree->SetBranchAddress("BToKpipi_Kpi_mass",&BToKpipi_Kpi_mass);
-  _tree->SetBranchAddress("BToKpipi_mass",&BToKpipi_mass);
-  _tree->SetBranchAddress("BToKpipi_piBu_charge",&BToKpipi_piBu_charge);
-  _tree->SetBranchAddress("BToKpipi_piBu_pt",&BToKpipi_piBu_pt);
-  _tree->SetBranchAddress("BToKpipi_piBu_eta",&BToKpipi_piBu_eta);
-  _tree->SetBranchAddress("BToKpipi_piBu_phi",&BToKpipi_piBu_phi);
-  _tree->SetBranchAddress("BToKpipi_kaon_pt",&BToKpipi_kaon_pt);
-  _tree->SetBranchAddress("BToKpipi_kaon_eta",&BToKpipi_kaon_eta);
-  _tree->SetBranchAddress("BToKpipi_kaon_phi",&BToKpipi_kaon_phi);
-  _tree->SetBranchAddress("BToKpipi_piD0_pt",&BToKpipi_piD0_pt);
-  _tree->SetBranchAddress("BToKpipi_piD0_eta",&BToKpipi_piD0_eta);
-  _tree->SetBranchAddress("BToKpipi_piD0_phi",&BToKpipi_piD0_phi);
+  int BToKpipi_info = _tree->SetBranchAddress("nBToKpipi",&nBToKpipi);
+  if(BToKpipi_info>=0){
+    _tree->SetBranchAddress("BToKpipi_CL_vtx",&BToKpipi_CL_vtx);
+    _tree->SetBranchAddress("BToKpipi_Kpi_CL_vtx",&BToKpipi_Kpi_CL_vtx);
+    _tree->SetBranchAddress("BToKpipi_Kpi_mass",&BToKpipi_Kpi_mass);
+    _tree->SetBranchAddress("BToKpipi_mass",&BToKpipi_mass);
+    _tree->SetBranchAddress("BToKpipi_piBu_charge",&BToKpipi_piBu_charge);
+    _tree->SetBranchAddress("BToKpipi_piBu_pt",&BToKpipi_piBu_pt);
+    _tree->SetBranchAddress("BToKpipi_piBu_eta",&BToKpipi_piBu_eta);
+    _tree->SetBranchAddress("BToKpipi_piBu_phi",&BToKpipi_piBu_phi);
+    _tree->SetBranchAddress("BToKpipi_kaon_pt",&BToKpipi_kaon_pt);
+    _tree->SetBranchAddress("BToKpipi_kaon_eta",&BToKpipi_kaon_eta);
+    _tree->SetBranchAddress("BToKpipi_kaon_phi",&BToKpipi_kaon_phi);
+    _tree->SetBranchAddress("BToKpipi_piD0_pt",&BToKpipi_piD0_pt);
+    _tree->SetBranchAddress("BToKpipi_piD0_eta",&BToKpipi_piD0_eta);
+    _tree->SetBranchAddress("BToKpipi_piD0_phi",&BToKpipi_piD0_phi);
+  }
 
-  _tree->SetBranchAddress("nGenPart",&nGenPart);
-  _tree->SetBranchAddress("GenPart_pdgId",&GenPart_pdgId);
-  _tree->SetBranchAddress("GenPart_genPartIdxMother",&GenPart_genPartIdxMother);
-  _tree->SetBranchAddress("GenPart_pt",&GenPart_pt);
-  _tree->SetBranchAddress("GenPart_eta",&GenPart_phi);
+  int BToKmumu_info = _tree->SetBranchAddress("nBToKmumu",&nBToKmumu);
+  if(BToKmumu_info>=0){
+    _tree->SetBranchAddress("BToKmumu_CL_vtx",&BToKmumu_CL_vtx);
+    _tree->SetBranchAddress("BToKmumu_mumu_CL_vtx",&BToKmumu_mumu_CL_vtx);
+    _tree->SetBranchAddress("BToKmumu_mumu_mass",&BToKmumu_mumu_mass);
+    _tree->SetBranchAddress("BToKmumu_mass",&BToKmumu_mass);
+    _tree->SetBranchAddress("BToKmumu_kaon_charge",&BToKmumu_kaon_charge);
+    _tree->SetBranchAddress("BToKmumu_kaon_pt",&BToKmumu_kaon_pt);
+    _tree->SetBranchAddress("BToKmumu_kaon_eta",&BToKmumu_kaon_eta);
+    _tree->SetBranchAddress("BToKmumu_kaon_phi",&BToKmumu_kaon_phi);
+    _tree->SetBranchAddress("BToKmumu_mu1_pt",&BToKmumu_mu1_pt);
+    _tree->SetBranchAddress("BToKmumu_mu1_eta",&BToKmumu_mu1_eta);
+    _tree->SetBranchAddress("BToKmumu_mu1_phi",&BToKmumu_mu1_phi);
+    _tree->SetBranchAddress("BToKmumu_mu2_pt",&BToKmumu_mu2_pt);
+    _tree->SetBranchAddress("BToKmumu_mu2_eta",&BToKmumu_mu2_eta);
+    _tree->SetBranchAddress("BToKmumu_mu2_phi",&BToKmumu_mu2_phi);
+  }
+
+  int BToKee_info = _tree->SetBranchAddress("nBToKee",&nBToKee);
+  if(BToKee_info>=0){
+    _tree->SetBranchAddress("BToKee_CL_vtx",&BToKee_CL_vtx);
+    _tree->SetBranchAddress("BToKee_mumu_CL_vtx",&BToKee_ee_CL_vtx);
+    _tree->SetBranchAddress("BToKee_mumu_mass",&BToKee_ee_mass);
+    _tree->SetBranchAddress("BToKee_mass",&BToKee_mass);
+    _tree->SetBranchAddress("BToKee_kaon_charge",&BToKee_kaon_charge);
+    _tree->SetBranchAddress("BToKee_kaon_pt",&BToKee_kaon_pt);
+    _tree->SetBranchAddress("BToKee_kaon_eta",&BToKee_kaon_eta);
+    _tree->SetBranchAddress("BToKee_kaon_phi",&BToKee_kaon_phi);
+    _tree->SetBranchAddress("BToKee_ele1_pt",&BToKee_ele1_pt);
+    _tree->SetBranchAddress("BToKee_ele1_eta",&BToKee_ele1_eta);
+    _tree->SetBranchAddress("BToKee_ele1_phi",&BToKee_ele1_phi);
+    _tree->SetBranchAddress("BToKee_ele2_pt",&BToKee_ele2_pt);
+    _tree->SetBranchAddress("BToKee_ele2_eta",&BToKee_ele2_eta);
+    _tree->SetBranchAddress("BToKee_ele2_phi",&BToKee_ele2_phi);
+  }
+
+  bool isMC = _tree->SetBranchAddress("nGenPart",&nGenPart);
+  if(isMC){
+    _tree->SetBranchAddress("GenPart_pdgId",&GenPart_pdgId);
+    _tree->SetBranchAddress("GenPart_genPartIdxMother",&GenPart_genPartIdxMother);
+    _tree->SetBranchAddress("GenPart_pt",&GenPart_pt);
+    _tree->SetBranchAddress("GenPart_eta",&GenPart_phi);
+  }
 
 }
 
